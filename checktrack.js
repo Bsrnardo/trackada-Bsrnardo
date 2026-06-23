7a01f2ed6e0106b260767746d3c5e39551622f3f
Bernardo Sanchez Zapata <brnrdsz@gmail.com>
2026-06-23 20:18:47 +0200

    Ligne Repository Git

--- checktrack.js
+++ checktrack.js
@@ -1,25 +1,18 @@
-import { readFileSync, existsSync } from "fs"
+import { readFileSync, existsSync } from "node:fs"
 import { join } from "path"
 import { homedir } from "os"
 import { log } from "console";
 
 
-// Parse le fichier JSON
-
+// Charger et parser le fichier JSON
 const track = JSON.parse (readFileSync("track.json", "utf-8"));
 console.log(track);
 
-// Verifier la presence du dossier ada-tech
-
+// Verifier dossier ada-tech exist ~
 const cheminAda = join(homedir(), "ada-tech")
 console.log(cheminAda);
-
-const adaVerif = existsSync(cheminAda); // Retourne true ou false
-if (adaVerif) {
-    console.log(" ✅ Il existe");
-} else {
-    console.log(" ❌ Il n'existe pas");
-};
+const adaVerif = existsSync(cheminAda);
+console.log(adaVerif ? "✅ Il existe" : "❌ Il n'existe pas");
 
 // Recuperer tout les projets du fichier JSON
 // Iterer sur les projets
@@ -28,11 +21,13 @@ if (adaVerif) {
 track.projects.forEach((project) => {
     const cheminProject = join(homedir(), "ada-tech", "bloc1", "projets", project.name );
     const dossierVerif = existsSync(cheminProject);
-    if (dossierVerif) {
-        console.log(`✅ ${project.name} - dossier trouvé`)
-    } else {
-        console.log(`❌ ${project.name} - dossier inexistant`)
-    }
+    console.log(dossierVerif ? `${project.name} - ✅ dossier trouvé` : `❌ ${project.name} - dossier inexistant`);
+
+    // Vérifier que chaque projet est initialisé comme un repository Git
+    const cheminGit = join(cheminProject, ".git");
+    const gitExiste = dossierVerif && existsSync(cheminGit);
+    console.log(gitExiste ? `  OK ${project.name} - Git initialisé` : `  NOT OK ${project.name} - Pas de Git`);
     });
 
-    
+
+
