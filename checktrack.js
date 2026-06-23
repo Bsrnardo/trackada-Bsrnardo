ca26f70c9fc1366fb2e3c968d81727c08d5dd184
Bernardo Sanchez Zapata <brnrdsz@gmail.com>
2026-06-23 12:25:51 +0200

    Changement placement dossier et adresse

--- checktrack.js
+++ checktrack.js
@@ -8,3 +8,26 @@ import { homedir } from "os"
 const track = JSON.parse (readFileSync("track.json", "utf-8"))
 
 console.log(track);
+
+// Verifier la presence du dossier ada
+
+const cheminAda = join(homedir(), "ada-tech")
+console.log(cheminAda);
+
+const adaVerif = existsSync(cheminAda) // Retourne true ou false
+
+if (adaVerif) {
+    console.log("Il existe");
+} else {
+    console.log("Il n'existe pas");
+};
+
+// Recuperer tout les projets du fichier JSON
+// Iterer sur les projets
+// Verifier si chacun des projets exist ou non
+
+track.projects.forEach((project) => {
+    console.log(project.name);
+    const cheminProject = join(homedir(), "ada-tech", "bloc-1", "projets", "trackada", project.name )
+    
+});
